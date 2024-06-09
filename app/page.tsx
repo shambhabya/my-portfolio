119022a7290eee423ecc8520edb013bbced9b56e
shambhabya <shambhabyamedhi@gmail.com>
2024-06-09 19:10:58 +0530

    removed locomotive scroll

--- app/page.tsx
+++ app/page.tsx
@@ -5,41 +5,18 @@ import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero/Hero";
 import About from "@/components/about/About";
 import Experience from "@/components/experience/Experience";
-import LocomotiveScroll from "locomotive-scroll";
 import Projects from "@/components/projects/Projects";
-import Test from "@/components/test/Test";
 
 export default function Home() {
-  useEffect(() => {
-    async function getLocomotive() {
-      const scrollContainer = document.querySelector("[data-scroll-container]");
-
-      if (scrollContainer) {
-        const scroll = new LocomotiveScroll({
-          el: scrollContainer as HTMLElement,
-          smooth: true,
-        });
-      } else {
-        console.error("Scroll container not found");
-      }
-    }
-
-    getLocomotive();
-  }, []);
-
   return (
-    <main style={{}} className="main " data-scroll-container>
+    <main style={{}} className="main ">
       <Navbar />
 
       <div id="shambhabya" style={{}} className="">
         <Hero />
       </div>
 
-      {/* <div className="h-screen bg-cyan-200">
-        <Test />
-      </div> */}
-
-      <div id="about" style={{}} data-scroll-section>
+      <div id="about" style={{}}>
         <About />
       </div>
 
